e6f9c40e79a5ea8283e27a231ce26587ca81f09b
Taka0920610 <qiaojixinmei76@gmail.com>
2025-10-16 15:48:51 +0900

    エラー修正

--- diaries/NiimiDiary.py
+++ diaries/NiimiDiary.py
@@ -4,6 +4,6 @@ class NiimiDiary(AbstractDiary):
     def get_date(self):
         return "2025-10-16"
     def get_summary(self):
-        return "GitHubがとても難しかった"
+        return "GitHubの使い方を学んだが全然わからなかった"
     def get_author(self):
-        return "Sample"
+        return "新美"
